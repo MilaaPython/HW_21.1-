081f3890fade137aafa35f4cd9703cbf5235752f
mila <125148912+MilaaPython@users.noreply.github.com>
2023-08-22 11:13:35 +0300

    Update admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,15 +1,25 @@
 from django.contrib import admin
-
-from catalog.models import Category, Product
+from .models import Category, Product, Contacts, Post
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'category_name',)
+    list_display = ('id', 'name',)
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'product_name', 'product_cost', 'product_category')
-    list_filter = ('product_category',)
-    search_fields = ('product_name', 'product_description',)
+    list_display = ('id', 'name', 'description', 'price', 'category', 'date_create')
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
+    date_hierarchy = 'date_create'
+
+
+@admin.register(Contacts)
+class ContactsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'phone', 'email',)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created', 'published', 'views')
